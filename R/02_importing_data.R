d08907436eea4d4acb12dd34060c0fd602d6196a
thalesdrago <thalesdrago@gmail.com>
2022-07-12 11:40:42 -0300

    Commenting the script, explaining what the code does

--- R/02_importing_data.R
+++ R/02_importing_data.R
@@ -4,42 +4,47 @@
 
 # Reading data -----------------------------------------------------------------
 
-# Listing all files
+# Listing all files that end with csv and returning the full names
 
 cestes_files <- list.files(path = "data/raw/cestes",
                            pattern = "csv$",
                            full.names = TRUE)
 
+# Creating a vector that has the names of the files without the .csv in the end
 cestes_names <- gsub(".csv", "", basename(cestes_files), fixed = TRUE)
 
+#Reading environmental file
 envir <- read.csv(cestes_files[3])
 
+#Applying to each element of the vector of files the function read.csv and creating a list
 data_list <- lapply(cestes_files, read.csv)
-names(data_list) <- cestes_names
+names(data_list) <- cestes_names #Naming each element of the list with the names of the vector of names
 
 length(data_list)
 
 # Inspecting data
 head(data_list$envir)
 dim(data_list$envir)
-summary(envir$envir)
+summary(data_list$envir)
 
 # Output 1: summary table ------------------------------------------------------
 
 # Creating summary table for all environmental variables
 sd(envir$Clay)
-envir_mean <- apply(envir[, -1], 2, mean)
+envir_mean <- apply(envir[, -1], 2, mean) # applying the function mean to each collunm, but the first, of the envir dataframe
 envir_sd <- apply(envir[, -1], 2, sd)
 
 # Creating a function in R -----------------------------------------------------
+# This function calculates the standard error of a vector
 std <- function(x, round = FALSE, ...) {
   std <- sd(x) / sqrt(length(x))
-  if (round) std <- round(std, ...)
+  if (round) std <- round(std, ...) #the ... let the user determine in the std function the arguments of the round function
   return(std)
 }
 
 std(envir$Clay, round = TRUE, digits = 2)
 
+#Applying the std function to all the collunms of envir dataframe, but the first
 envir_std <- apply(envir[, -1], 2, std, round = TRUE, digits = 2)
 
 envir_tbl <- data.frame(variable = names(envir_mean),
